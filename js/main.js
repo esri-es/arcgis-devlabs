ac27b0967eab6f769278c22b2030251fe1521b72
Raul Jimenez Ortega <hhkaos@gmail.com>
2015-09-22 11:33:22 +0200

    Adding UI to add comments

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 "use strict";
 
-var tutorialsApp = angular.module('tutorialsApp', ['ngSanitize']);
+var tutorialsApp = angular.module('tutorialsApp', ['ngSanitize', 'LocalStorageModule']);
 
-tutorialsApp.controller('MainCtrl', ['$scope', '$http', '$sce',
-    function ($scope, $http, $sce) {
+tutorialsApp.controller('MainCtrl', ['$scope', '$http', '$sce', '$location', 'localStorageService',
+    function ($scope, $http, $sce, $location, localStorageService) {
 
+        function getParameterByName(name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
 
-        $http.get('https://api.github.com/repos/esri-es/JavascriptAPI/issues',
-            {headers: {"Authorization": "token b751cfeb0f636c9474739f7da2f07a3936d0eec3"}}).
+        $scope.access_token = getParameterByName("access_token");
+        if($scope.access_token){
+            localStorageService.set('access_token', $scope.access_token);
+        }
+
+        $scope.access_token = localStorageService.get('access_token');
+
+        $http.defaults.headers.common.Authorization = 'token ' + $scope.access_token;
+
+        $http.get('https://api.github.com/repos/esri-es/JavascriptAPI/issues').
             then(function (response) {
                 $scope.issues = response.data;
             });
 
-        $http.get('https://api.github.com/repos/esri-es/JavascriptAPI/contents/src/tutoriales',
-            {headers: {"Authorization": "token b751cfeb0f636c9474739f7da2f07a3936d0eec3"}}).
+        $http.get('https://api.github.com/repos/esri-es/JavascriptAPI/contents/src/tutoriales').
             then(function (response) {
                 $scope.tutorials = [];
                 response.data.forEach(function (elem) {
@@ -44,14 +57,35 @@ tutorialsApp.controller('MainCtrl', ['$scope', '$http', '$sce',
 
             });
 
+        $scope.submit = function(){
+            if($scope.question){
+                $http.post('https://api.github.com/repos/esri-es/JavascriptAPI/issues',
+                    {
+                        "title": $scope.tutoTitle,
+                        "body": $scope.question
+                    }).then(function(response) {
+                        if(response.statusText === "Created"){
+                            $scope.issues.push(response.data);
+                        }
+                });
+            }else{
+                alert("Introduce tu pregunta antes de enviar");
+            }
+        };
+
+        $scope.close = function(){
+            localStorageService.set('access_token', '');
+            delete $scope.access_token;
+        };
+
         $scope.update = function () {
             var elem = $scope.tutorials[$scope.tuto];
+            $scope.tutoTitle = $scope.tutorials[$scope.tuto].name;
             $scope.html_url = elem.url;
             $scope.url = elem.path;
 
 
-            $http.get("./" + $scope.url,
-                {headers: {"Authorization": "token b751cfeb0f636c9474739f7da2f07a3936d0eec3"}}).
+            $http.get("./" + $scope.url).
                 then(function (response) {
                     $scope.htmlCode = response.data;
 
