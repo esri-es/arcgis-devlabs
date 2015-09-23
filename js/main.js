de8ca7ab35b44ebf2b751dd78625e18a28b0a9d8
Raul Jimenez Ortega <hhkaos@gmail.com>
2015-09-23 17:01:40 +0200

    Fixing add to favourites

--- js/main.js
+++ js/main.js
@@ -121,13 +121,15 @@ tutorialsApp.controller('MainCtrl', ['$scope', '$http', '$sce', '$location', 'lo
         };
 
         $scope.addToFavourites = function(){
-            if (window.sidebar) { // Mozilla Firefox Bookmark
-                window.sidebar.addPanel(location.href,document.title,"");
-            } else if(window.external) { // IE Favorite
-                window.external.AddFavorite(location.href,document.title); }
-            else if(window.opera && window.print) { // Opera Hotlist
-                this.title = document.title;
+            if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+                window.sidebar.addPanel(document.title,window.location.href,'');
+            } else if(window.external && ('AddFavorite' in window.external)) { // IE Favorite
+                window.external.AddFavorite(location.href,document.title);
+            } else if(window.opera && window.print) { // Opera Hotlist
+                this.title=document.title;
                 return true;
+            } else { // webkit - safari/chrome
+                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
             }
         };
 
